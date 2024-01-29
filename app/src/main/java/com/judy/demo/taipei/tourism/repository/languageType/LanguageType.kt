4ecd98b1218c8fda36083cf7af8718dc074782f4
Chan Shiang-Yun <cloudfly4326@gmail.com>
2024-01-29 18:30:35 +0800

    新增語系類型 enum class

---
+++ app/src/main/java/com/judy/demo/taipei/tourism/repository/languageType/LanguageType.kt
@@ -0,0 +1,23 @@
+package com.judy.demo.taipei.tourism.repository.languageType
+
+/** 語言類型 */
+enum class LanguageType {
+    /**正體中文*/
+    ZH_TW ,
+    /**簡體中文*/
+    ZH_CN,
+    /**英文*/
+    EN,
+    /**日文*/
+    JA,
+    /**韓文*/
+    KO,
+    /**西班牙文*/
+    ES,
+    /**印尼文*/
+    ID,
+    /**泰文*/
+    TH,
+    /**越南文*/
+    VI
+}
